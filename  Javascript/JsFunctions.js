3f05b4bb3844fb2c6981ade8966e527b0a2e9bff
Steven Marshall <stevenmarshall1@hotmail.com>
2023-12-04 21:33:44 +0000

    commit

---  Javascript/JsFunctions.js
+++  Javascript/JsFunctions.js
@@ -73,3 +73,19 @@ Don't forget to comment your code. */
 
 
 
+function isPrime(number) {
+  if (number < 2) {
+      return false;
+  } else if (number === 2 || number === 3 || number === 5 || number === 7) {
+      return true;
+  } else if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0 || number % 7 === 0) {
+      return false;
+  } else {
+      return true;
+  }
+}
+
+let userInput = prompt("Pick a positive whole number");
+let number = parseInt(userInput);
+let result = isPrime(number);
+console.log(`The number ${number} is ${result ? "a prime number." : "not a prime number."}`);
