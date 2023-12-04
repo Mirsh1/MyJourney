9dde241fc9da7a35f8bd59eacd97d1e9dd95dd02
Steven Marshall <stevenmarshall1@hotmail.com>
2023-12-04 21:07:57 +0000

    college updates

---  Javascript/JsFunctions.js
+++  Javascript/JsFunctions.js
@@ -55,3 +55,21 @@ function showValue(){
 
 const result = calcFactorial(n);
 console.log(`The factorial of ${n} is:`, n * calcFactorial(n - 1));
+
+/* Activity 3 - Vanilla JS Functions
+Write a JavaScript function that takes a number as a parameter and check the number is prime or not. 
+
+Note: 
+
+Prime numbers are the numbers that are only divisible by themselves and 1, 
+in other words, if we try to divide them by another number, the result is not a whole number. 
+
+So, if we divide the number by anything other than 1 or itself, we will get a remainder that is not zero. 
+
+Negative numbers can not be prime.
+
+Important:
+Don't forget to comment your code. */ 
+
+
+
