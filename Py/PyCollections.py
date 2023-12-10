643f9e8dc6eda7c9e6310d83403df671c300ee9d
Steven Marshall <stevenmarshall1@hotmail.com>
2023-12-10 21:45:49 +0000

    final coll activity

--- Py/PyCollections.py
+++ Py/PyCollections.py
@@ -33,3 +33,36 @@ myList = [20, 30, 25, 35, -16, 60, -100]
 x = sum(myList)/len(myList)
 
 print("Average value of the list elements is ", sum(myList))
+
+
+"""
+Write a Python program to find the maximum and minimum value of a list.
+
+Test Data:
+
+Sample List:
+
+[25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+Expected Output:
+
+Original List: [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+Maximum value for the above list = 77
+
+Minimum value for the above list = 14
+
+Important:
+Don't forget to comment your code.
+
+
+"""
+
+list3 = [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+
+print(" Original List: ", list3)
+
+print ("Maximum value for the above list = ", max(list3) )
+
+print ("Minimum value for the above list = ", min(list3) )
